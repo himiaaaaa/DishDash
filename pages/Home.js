76579f975a78cdac3bc12aa815a9960138e73e99
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-03-10 12:28:17 +0200

    header

---
+++ pages/Home.js
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+import HomeHeader from '../components/HomeHeader.js'
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <HomeHeader />
+    </SafeAreaView>
+  )
+}

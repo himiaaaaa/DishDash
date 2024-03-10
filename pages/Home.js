892562b98f7b852032993ad2e03e5dca97b1b773
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-03-10 12:49:37 +0200

    update home style

--- pages/Home.js
+++ pages/Home.js
@@ -4,8 +4,10 @@ import HomeHeader from '../components/HomeHeader.js'
 
 export default function Home() {
   return (
-    <SafeAreaView>
-      <HomeHeader />
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+        <View style={{ backgroundColor: "white", padding: 15 }}>
+           <HomeHeader />
+        </View>
     </SafeAreaView>
   )
 }

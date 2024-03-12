6e62b44d9eaf3385272fc1e629eea17022382689
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-03-12 11:02:17 +0200

    update homeheader

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,21 +1,23 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React, {useState} from 'react'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
 
-export default function header() {
-  const [activeTab, setActiveTab] = useState('delivery')
+export default function header({city}) {
 
   return (
-    <View className="flex-row self-center">
-      <HeaderButton 
-        text="delivery" 
-        activeTab={activeTab}
-        setActiveTab={setActiveTab}
-      />
-      <HeaderButton 
-        text="pickup" 
-        activeTab={activeTab}
-        setActiveTab={setActiveTab}
-      />
+    <View className="flex-row justify-between">
+
+      {/* city display */}
+      <View className='flex-row ml-3'>
+        <Image source={require('../assets/images/nearby.png')} className='color-primary h-6 w-6 mt-1'/>
+        <View className="bg-primary mx-2 py-2 px-3 rounded-lg">
+          <Text className='text-white font-semibold text-15'>{city}</Text>
+        </View>
+      </View>
+
+      {/* shpping basket */}
+      <TouchableOpacity className='mr-3'>
+        <Image source={require('../assets/images/shopping-basket.png')} className='color-primary h-6 w-6 mt-1'/>
+      </TouchableOpacity>
     </View>
   )
 }

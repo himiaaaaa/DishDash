7e85b43b75a5ea74f74b05129aa472c9d2a3cde6
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-03-13 10:45:20 +0200

    Delete unused code

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -21,16 +21,3 @@ export default function header({city}) {
     </View>
   )
 }
-
-const HeaderButton = (props) => (
-    <TouchableOpacity 
-      className={`bg-${props.activeTab === props.text ? 'primary' : 'white'} mx-1 py-2 px-5 rounded-full`}
-      onPress={() => props.setActiveTab(props.text)}
-    >
-      <Text 
-        className={`text-${props.activeTab === props.text ? 'white' : 'black'} font-semibold text-15`}
-      >
-        {props.text}
-      </Text>
-    </TouchableOpacity>
-)

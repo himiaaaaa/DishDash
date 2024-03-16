d02b9c6a7a0bec8d8dd21708d65d260a0995b0d8
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-03-16 11:33:48 +0200

    fix bug

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -8,7 +8,7 @@ export default function header({city}) {
 
       {/* city display */}
       <View className='flex-row ml-3'>
-        <Image source={require('../assets/images/nearby.png')} className='color-primary h-6 w-6 mt-1'/>
+        <Image source={require('../assets/images/nearby.png')} className='tintColor-primary h-6 w-6 mt-1'/>
         <View className="bg-primary mx-2 py-2 px-3 rounded-lg">
           <Text className='text-white font-semibold text-15'>{city}</Text>
         </View>
@@ -16,7 +16,7 @@ export default function header({city}) {
 
       {/* shpping basket */}
       <TouchableOpacity className='mr-3'>
-        <Image source={require('../assets/images/shopping-basket.png')} className='color-primary h-6 w-6 mt-1'/>
+        <Image source={require('../assets/images/shopping-basket.png')} className='tintColor-primary h-6 w-6 mt-1'/>
       </TouchableOpacity>
     </View>
   )

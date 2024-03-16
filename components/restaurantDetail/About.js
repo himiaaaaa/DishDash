5c35db2017174fc6bf586b11f5dc49a47d3f789d
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-03-16 16:37:47 +0200

    add about part in restaurant detail

--- components/restaurantDetail/About.js
+++ components/restaurantDetail/About.js
@@ -1,11 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text, Image } from "react-native";
 
-export default function About({ route, navigation }) {
-  const { name } = route.params.restaurant;
+export default function About({ route }) {
+  const { name, image_url, price, rating, categories } = route.params.restaurant;
+
+  const dotCategories = categories.map((c) => c.title).join(" • ");
+
+  const pricePlusRating = `${rating} ⭐ • ${ price ?? ""}` ;
+
   return (
-    <View className='mt-30'>
-      <Text className='color-red'>{name}</Text>
+    <View>
+
+      <Image 
+        source={{ uri: image_url || 'https://images.unsplash.com/photo-1537047902294-62a40c20a6ae?q=80&w=2535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }} 
+        className="w-full h-48"
+      />
+
+      
+      <Text className="uppercase first-letter:text-3xl -mt-8 px-4 text-white border-primary font-extrabold">
+        {name}
+      </Text> 
+
+      <Text className="mt-2 mx-4 text-base font-normal">
+        {dotCategories}
+        {"\n"}
+        {pricePlusRating}
+      </Text>
+
     </View>
-  )
+  );
 }
+

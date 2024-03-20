cb62d7a4933f05dc5f965e66eb52059525cc775d
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-03-20 12:53:16 +0200

    update about.js note

--- components/restaurantDetail/About.js
+++ components/restaurantDetail/About.js
@@ -9,32 +9,32 @@ export default function About({ route }) {
   return (
     <View>
 
+      {/* header image */}
       <Image 
         source={{ uri: image_url || 'https://images.unsplash.com/photo-1537047902294-62a40c20a6ae?q=80&w=2535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }} 
         className="w-full h-48"
       />
 
-    <View className='flex-row space-x-3 -mt-7 bg-white rounded-tr-full w-2/3 justify-center'>
-      <Text className="uppercase text-xl font-medium">
-        {name}
-      </Text> 
-      <Text className="text-xl font-medium">
-       {ratingStar}
-      </Text>
-    </View>
-  
-    <View className='flex-row mb-5'>
-      {categories?.map((category, index) => {
-          return (
-              <Text key={index} className='bg-primary ml-2 mt-3 p-1 text-s text-white font-medium shadow-sm shadow-primary'>
-                {category.title}
-              </Text>  
-          )
-      })}
-    </View>
-      
-     
-
+      {/* header info */}
+      <View className='flex-row space-x-3 -mt-7 bg-white rounded-tr-full w-2/3 justify-center'>
+        <Text className="uppercase text-xl font-medium">
+          {name}
+        </Text> 
+        <Text className="text-xl font-medium">
+         {ratingStar}
+        </Text>
+      </View>
+    
+      <View className='flex-row mb-5'>
+        {categories?.map((category, index) => {
+            return (
+                <Text key={index} className='bg-primary ml-2 mt-3 p-1 text-s text-white font-medium shadow-sm shadow-primary'>
+                  {category.title}
+                </Text>  
+            )
+        })}
+      </View>
+    
     </View>
   );
 }

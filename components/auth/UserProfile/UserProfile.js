c7c698bc421882f98def3e6f49fbfce76a610413
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-05-06 12:48:10 +0300

    fix userprofile

--- components/auth/UserProfile/UserProfile.js
+++ components/auth/UserProfile/UserProfile.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { handleSignOut } from '../../../services/authService';
-import welcome from '../../assets/images/welcome.png';
+import welcome from '../../../assets/images/welcome.png';
 
 const UserProfile = ({ user, auth, navigation }) => {
     const dispatch = useDispatch();
